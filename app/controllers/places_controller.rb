7b63a568a2695c8d49532c6556800cbdeb9b8b95
Shanna Kershner <shanna.kershner@gmail.com>
2019-08-27 01:33:32 +0000

    page limit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.paginate(page: params[:page], per_page: 1)
+    @places = Place.paginate(page: params[:page], per_page: 10)
   end
 
 end
