0d47e8dcde9db5e99d421163d8ea991f40cd5f0f
Shanna Kershner <shanna.kershner@gmail.com>
2019-09-14 23:39:28 +0000

    fixed email notifications

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,8 @@
 class NotificationMailer < ApplicationMailer
-  default from: "no-reply@nomsterapp.com"
+  default from: "noreply@nomsterapp.com"
 
   def comment_added
-    mail(to: "gnoshferatu@gmail.com",
+    mail(to: "noreply.sk.19@gmail.com",
           subject: "comment added on place!")
   end
 end
